143f8705158b4cddee117d500fabf21a376a4314
mallikapps <mallik.techno@gmail.com>
2022-05-18 11:27:21 +0530

    Add project files.

---
+++ ConForJnk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConForJnk
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
